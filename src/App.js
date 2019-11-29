5b65d827af6ec9a562517411ee6a9c8ee2cf5d23
unknown <poslovnogacic@gmail.com>
2019-11-29 04:38:26 +0100

    menu bar

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./App.scss";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="wrapper">
+            <div className="home">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about-us" component={About} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/contact-us" component={Contact} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <p>About us</p>;
+}
+
+function Projects() {
+  return <p>Our Projects.</p>;
+}
+
+function Contact() {
+  return <p>Feel free to reach us.</p>;
+}
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h5>
+          The <b>SPRIGGAN 12</b>, is a creative, engineer driven, global agency
+          working on advancing the software, advertising and design communities
+          to new heights.
+        </h5>
+      </div>
+    </div>
+  );
+}
+export default App;
